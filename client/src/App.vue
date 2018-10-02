e0cd4a4c16d953c8a9b14544de914473193d43d2
auredentan <aurelien.dentan@gmail.com>
2018-10-02 14:15:56 +0200

    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav';
+export default {
+  components : {
+    AppNav
+  },
+};
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to my forum</h1>
+      <p class="lead">This is a simple forum made with vue.js, express, Node, PostGres and Bootstrap</p>
+      <hr class="my-4">
+      <p>Enjoy !</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+      </p>
+</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
